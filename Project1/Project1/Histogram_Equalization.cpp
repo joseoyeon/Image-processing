52027648abd280f78e319ecfb68f8addb8bf8ea7
joseoyeon <joseoyeon60@gmail.com>
2020-06-25 15:38:24 +0900

    push

---
+++ Project1/Project1/Histogram_Equalization.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+void imhist(Mat image, int histogram[]){
+
+    for (int i = 0; i < 256; i++){
+        histogram[i] = 0;
+    }
+
+    for (int y = 0; y < image.rows; y++)
+        for (int x = 0; x < image.cols; x++)
+            histogram[(int)image.at<uchar>(y, x)]++;
+}
+
+void sumhist(int histogram[], int sumhistogram[]){
+    sumhistogram[0] = histogram[0];
+    for (int i = 1; i < 256; i++){
+        sumhistogram[i] = histogram[i] + sumhistogram[i - 1];
+    }
+}
+
+void matchHistogram(int srcHist[256], int refHist[256], int map[256]) {
+    double cumSrc[256], cumRef[256];
+    cumSrc[0] = srcHist[0];
+    cumRef[0] = refHist[0];
+    for (int i = 1; i < 256; i++) {
+        cumSrc[0] = cumSrc[i - 1] + srcHist[i];
+        cumRef[0] = cumRef[i - 1] + refHist[i];
+    }
+    for (int i = 0; i < 256; i ++ ) {
+        cumSrc[i] /= cumSrc[255];
+        cumRef[i] /= cumRef[255];
+    }
+    for (int i = 0; i < 256; i++) {
+        int k = 255;
+        while (cumRef[k] > cumSrc[i]) k--;
+        map[i] = k < 0 ? 0 : k;
+    }
+}
+int main(){
+    Mat image = imread("project02\\Phobos1.png", IMREAD_GRAYSCALE);
+    Mat target_image = imread("project02\\Phobos2.png", IMREAD_GRAYSCALE);
+
+    int histogram[256];
+    imhist(image, histogram); // 이미지 히스토그램 분포 확인
+
+    int size = image.rows * image.cols;
+    float alpha = 255.0 / size;
+    
+    int sumhistogram[256];
+    sumhist(histogram, sumhistogram); // 시그마 nk
+
+    // Scale the histogram
+    int Sk[256];
+    for (int i = 0; i < 256; i++){
+        Sk[i] = cvRound((double)sumhistogram[i] * alpha); 
+        //시그마(nk/size) * 255 시그마 반올림해준다.
+    }
+
+    // Generate the equlized image
+    Mat new_image = image.clone();
+
+    for (int y = 0; y < image.rows; y++)
+        for (int x = 0; x < image.cols; x++)
+            new_image.at<uchar>(y, x) = (Sk[image.at<uchar>(y, x)]);
+
+    // Display the original Image
+    namedWindow("Original Image");
+    imshow("Original Image", image);
+
+    // Display equilized image
+    namedWindow("Equilized Image");
+    imshow("Equilized Image", new_image);
+
+    imwrite("img.jpg", image);
+    imwrite("img_result.jpg", new_image);
+    waitKey();
+    return 0;
+}
--- Project1/Project1/Histogram_Matching.cpp
+++ Project1/Project1/Histogram_Matching.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <time.h>
@@ -18,6 +18,45 @@ void imhist(Mat image, int histogram[]) {
 			histogram[(int)image.at<uchar>(y, x)]++;
 }
 
+void histDisplay(Mat image, const char* name)
+{
+	int histogram[256];
+	imhist(image, histogram);
+	int hist[256];
+	for (int i = 0; i < 256; i++)
+	{
+		hist[i] = histogram[i];
+	}
+	// draw the histograms
+	int hist_w = 512; int hist_h = 400;
+	int bin_w = cvRound((double)hist_w / 256);
+
+	Mat histImage(hist_h, hist_w, CV_8UC1, Scalar(255, 255, 255));
+
+	// find the maximum intensity element from histogram
+	int max = hist[0];
+	for (int i = 1; i < 256; i++) {
+		if (max < hist[i]) {
+			max = hist[i];
+		}
+	}
+
+	// normalize the histogram between 0 and histImage.rows
+
+	for (int i = 0; i < 256; i++) {
+		hist[i] = ((double)hist[i] / max) * histImage.rows;
+	}
+
+	// draw the intensity line for histogram
+	for (int i = 0; i < 256; i++)
+	{
+		line(histImage, Point(bin_w * (i), hist_h), Point(bin_w * (i), hist_h - hist[i]), Scalar(0, 0, 0), 1, 8, 0);
+	}
+
+	// display histogram
+	namedWindow(name, WINDOW_AUTOSIZE);
+	imshow(name, histImage);
+}
 
 // sumHist 로 histgram 분포를 구한다 .
 void sumhist(int histogram[], int sumhistogram[]) {
@@ -73,20 +112,23 @@ int main() {
 	Mat target_image = imread("project02\\Phobos2.png", IMREAD_GRAYSCALE);
 	Mat imageDst;
 
-	imageDst = Matching(image, target_image);
+	imageDst = Matching(image, target_image);  
 
 	// Display the original Image
 	namedWindow("Original Image");
 	imshow("Original Image", image);
+	histDisplay(image, "Original Image Histo");
 
 	// Display the original Image
 	namedWindow("Target Image");
 	imshow("Target Image", target_image);
+	histDisplay(target_image, "Target Image Histo");
 
 	// Display equilized image
 	namedWindow("Histogram Matching Image");
 	imshow("Histogram Matching Image", imageDst);
-
+	histDisplay(imageDst, "Histogram Matching Image Histo");
+	
 	imwrite("img_result.jpg", imageDst);
 	waitKey();
 	return 0;
