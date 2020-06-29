a525669553377c2eb4952128ec314f5b7b0ad25a
joseoyeon <joseoyeon60@gmail.com>
2020-06-29 20:22:02 +0900

    push

---
+++ 03. Project/main.cpp
@@ -0,0 +1,68 @@
+﻿#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+    const int mask[3][3] = {
+        {1,2,1},
+        {2,4,2},
+        {1,2,1}
+    };
+
+    const float fMaskTotalCoef = 1.f / 16.f;
+
+
+
+    Mat inputImage = imread("Lenna_gray.png", IMREAD_GRAYSCALE);  // Read the file
+    if (inputImage.empty())                              // Check for invalid input
+    {
+        cout << "Could not open or find the image" << std::endl;
+        return -1;
+    }
+    Mat outputImage = inputImage.clone();
+
+    CV_Assert(inputImage.depth() == CV_8U);
+    int nChannels = inputImage.channels();
+    int nRows = inputImage.rows;
+    int nCols = inputImage.cols * nChannels;
+
+    if (inputImage.isContinuous())
+    {
+        nCols *= nRows;
+        nRows = 1;
+    }
+
+    for (int i = 1; i < inputImage.rows - 1; ++i)
+    {
+        for (int j = 1; j < inputImage.cols - 1; ++j)
+        {
+            int nSum = 0;
+            for (int a = -1; a <= 1; ++a)
+            {
+                for (int b = -1; b <= 1; ++b)
+                {
+                    int intensity = inputImage.at<uchar>(i + a, j + b);
+                    int coefficient = mask[a + 1][b + 1];
+
+                    nSum += (intensity * coefficient);
+                }
+            }
+
+            nSum *= fMaskTotalCoef;
+            outputImage.at<uchar>(i, j) = uchar(nSum);
+        }
+    }
+
+    namedWindow("input image", WINDOW_AUTOSIZE); // Create a window for display
+    imshow("input image", inputImage);          // Show our image inside it.
+
+    namedWindow("output image", WINDOW_AUTOSIZE); // Create a window for display
+    imshow("output image", outputImage);          // Show our image inside it.
+    waitKey(0);                                     // Wait for a keystroke in the window
+
+    return 0;
+}
